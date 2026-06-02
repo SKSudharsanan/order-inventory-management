d9d8e6e60403b22216cd6050be9f16239c455f81
bear on chain <bearonchain@gmail.com>
2026-06-02 13:23:38 +0530

    fix: convert sqlx errors into app errors

--- src/errors.rs
+++ src/errors.rs
@@ -17,6 +17,9 @@ pub enum AppError {
 
     #[error("Internal server error")]
     InternalServerError,
+
+    #[error("Database error")]
+    DatabaseError(#[from] sqlx::Error),
 }
 
 #[derive(Serialize)]
@@ -28,6 +31,7 @@ pub struct ErrorResponse {
 impl IntoResponse for AppError {
     fn into_response(self) -> Response {
         let status_code = match self {
+            AppError::DatabaseError(_) => StatusCode::INTERNAL_SERVER_ERROR,
             AppError::BadRequest(_) => StatusCode::BAD_REQUEST,
             AppError::NotFound => StatusCode::NOT_FOUND,
             AppError::InternalServerError => StatusCode::INTERNAL_SERVER_ERROR,
